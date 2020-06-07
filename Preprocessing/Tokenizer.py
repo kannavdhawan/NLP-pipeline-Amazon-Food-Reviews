ce7083241668222bfa222ecda6dea874b3b8e3bc
Kannav Dhawan <kannavdhawan1@live.com>
2020-06-07 10:39:23 +0000

    preprocessing

--- Preprocessing/Tokenizer.py
+++ Preprocessing/Tokenizer.py
@@ -1,15 +1,17 @@
 import numpy as np
-import pandas as pd
 def tokenizer(pos,neg):
     tokenized_pos=[]
     tokenized_neg=[]
     tokens=[]
+    # lowercasing it as it won't omit words during stopword removal.
     pos=pos.lower()
     neg=neg.lower()
 # -------splitting by \n -- sentence tokenization-------
-    positive_reviews_split=pos.splitlines() # returns a list
+    
+    positive_reviews_split=pos.splitlines() # returns a list [Review 1, Review 2, Review 3, ...Review n]
     negative_reviews_split=neg.splitlines()
     # print(positive_reviews_split[1:3])
+
 #----word tokenization----
     for i in positive_reviews_split:
         pass
@@ -25,8 +27,4 @@ def tokenizer(pos,neg):
     print("Tokenized neg subset-->")
     print(tokenized_neg[0:3])
     tokens.append(tokenized_neg)
-
     return tokens
-
-
-
