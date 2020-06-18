cd84414a96eb9dfa1fe2e327021e7ac882bd11a8
Kannav Dhawan <kannavdhawan1@live.com>
2020-06-18 09:05:34 +0000

    word2vec

---
+++ A3/inference.py
@@ -0,0 +1,14 @@
+from gensim_w import load_model,most_sim
+import pandas as pd
+from gensim.models import Word2Vec
+import os
+import sys
+def main(txt_path):
+    with open(txt_path,'r') as f:
+        list_words=f.read().splitlines()
+    n=20
+    for word in list_words:
+        model=Word2Vec.load("data/word2vec.model")
+        most_sim(model,word,n)
+        
+
